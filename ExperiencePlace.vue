f5a9c454f838eebedb9d928356e2f97026004428
Arzuv Hudaynazarova <126810475+arzuv-hudaynazarova@users.noreply.github.com>
2023-12-12 23:15:42 +0300

    Add files via upload

---
+++ ExperiencePlace.vue
@@ -0,0 +1,24 @@
+<template>
+  <router-link
+      :to="{ name: 'experience.show', params: { experienceSlug: experience.slug } }"
+      class="card-link"
+  >
+    <div class="card">
+      <div class="card__image-container">
+        <img :src="`/images/${experience.image}`" :alt="experience.name" class="card__image" />
+      </div>
+      <div class="card__text-container">
+        <span class="card__text">
+          {{ experience.name }}</span>
+      </div>
+    </div>
+  </router-link>
+</template>
+<script>
+export default {
+  props: {
+    experience: { type: Object, required: true }
+  }
+}
+</script>
+
---
+++ TheNavigation.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="nav">
+    <AppLink id="logo" to="/">Travel's Trail</AppLink>
+    <AppLink
+        v-for="destination in destinations"
+        :key="destination.id"
+        :to="{name: 'destination.show', params:{id: destination.id, slug: destination.slug}}"
+    >
+      {{ destination.name }}
+    </AppLink>
+    <AppLink :to="{name: 'protected'}">Panel</AppLink>
+  </div>
+</template>
+<script>
+import sourceData from '@/data.json'
+export default {
+  data(){
+    return {
+      destinations: sourceData.destinations
+    }
+  }
+}
+</script>
